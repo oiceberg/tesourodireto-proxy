0a93cac3bec543d4395303ffa31a6d39ebd0ba60
O Iceberg <oiceberg@protonmail.com>
2025-09-07 01:55:34 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -27,4 +27,7 @@ app.get('/tesouro-csv', async (req, res) => {
     }
 });
 
-module.exports = app;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Proxy rodando na porta ${PORT}`);
+});
